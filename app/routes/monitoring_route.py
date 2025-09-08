8a42a5f341042958c49b196c5b58bbca31015a49
muhammadirfanbaihaqi <muhammadirfanbaihaqi538@gmail.com>
2025-09-08 14:45:39 +0700

    abaikan file teststr.html. membuat stream menjadi fungsional . 1 mengubah file monitoring.html , menambahkan 2 tombol baru dan animasi ketika user menekan masing-masing tombol, menghapus tombol lama . 2.mengedit file monitoring.css 3. mengedit file routes, untuk menambahkan link server/websocket wss yang terhubung ke rasp, bisa stream saat raspi on.

--- app/routes/monitoring_route.py
+++ app/routes/monitoring_route.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify, session
 from app.controller.user_controller import UserController
 from app.utils.auth_utils import web_session_required
+from config import Config
+import os
 
 # Create blueprint
 monitoring_bp = Blueprint('monitoring', __name__)
 
-# protected web session required
 @monitoring_bp.route('/monitoring')
 @web_session_required
 def monitoring():
-    """Monitoring page"""
-    return render_template('monitoring.html')
-
-# route function monitoring disini
+    # ambil ws_url dari config, atau set default
+    wss_url = os.getenv('NILOCAM_WSS_URL', 'wss://nilocam.my.id')
+    return render_template('monitoring.html', ws_url=wss_url)
